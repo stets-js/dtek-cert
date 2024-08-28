d08aa380c3e9124066d4c028eaaf789b2d68839c
Dmytro Stetsenko <97848708+stets-js@users.noreply.github.com>
2024-08-28 14:01:12 +0300

    add

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-TOKEN = "7061386650:AAHuEge8iJdl1nuhRtehZ8ek2Kbh_JBn9M0"
+TOKEN = "7205458935:AAFL5IUeTIlnNhZRLPSejxwV4kQsZpU7CIo"
 # ADMIN_ID = os.environ['ADMIN_ID']
 ADMIN_ID = "001"
